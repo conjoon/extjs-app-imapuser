ecb566e20bd331b907e8392a9ec1bcbbb9623235
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2020-04-01 09:03:29 +0200

    - enhancement: minor doc updates (see coon-js/app-cn_imapuser#4)

--- tests/src/UserProviderTest.js
+++ tests/src/UserProviderTest.js
@@ -1,7 +1,7 @@
 /**
  * conjoon
  * app-cn_imapuser
- *  Copyright (C) 2017 - 2020 Thorsten Suckow-Homberg https://github.com/conjoon/app-cn_imapuser
+ * Copyright (C) 2017 - 2020 Thorsten Suckow-Homberg https://github.com/conjoon/app-cn_imapuser
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
